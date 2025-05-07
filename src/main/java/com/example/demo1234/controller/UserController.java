9df4680b5e64838a49c2394f23e2dd99768e5ca7
Krekno <yigiterenkarabas@gmail.com>
2025-05-07 18:41:50 +0300

    fix(UserController): allow public access to update-profile

--- src/main/java/com/example/demo1234/controller/UserController.java
+++ src/main/java/com/example/demo1234/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.demo1234.controller;
 import com.example.demo1234.dto.UpdateProfileRequest;
 import com.example.demo1234.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +12,6 @@ public class UserController {
     private final UserService userService;
 
     @PutMapping("/update-profile")
-    @PreAuthorize("hasRole('CUSTOMER')")
     public String updateProfile(@RequestBody UpdateProfileRequest request) {
         userService.updateProfile(request);
         return "Profile updated successfully!";
