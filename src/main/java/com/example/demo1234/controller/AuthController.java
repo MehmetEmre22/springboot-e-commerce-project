793fb29ef64671475561074b4e55973542113160
Krekno <yigiterenkarabas@gmail.com>
2025-05-08 22:51:45 +0300

    Revert "fix(AuthController): remove role return from login response"
    This reverts commit a29dbbd5798d0512a6fafaa74a69bb9634feaea4.

--- src/main/java/com/example/demo1234/controller/AuthController.java
+++ src/main/java/com/example/demo1234/controller/AuthController.java
@@ -61,6 +61,6 @@ public class AuthController {
         // 3. Token oluşturuyoruz (email + rol ile)
         String token = jwtUtil.generateToken(user.getEmail(), user.getRole().name());
 
-        return new LoginResponse(token);
+        return new LoginResponse(token, user.getRole());
     }
 }
